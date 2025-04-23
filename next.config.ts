aa632ba58f24b7c717c122aa5c25fc6a858917e8
prantoshikder <prantoshikder24@gmail.com>
2025-04-23 22:25:01 +0600

    Set Up ESLint, Prettier, StyleLint, and lint-staged

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "*",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+        pathname: "**",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
+  distDir: ".next",
 };
 
 export default nextConfig;
